d70aad2f0ef6b57da61cdf536199f11ce263bf0d
Tommy Oliver <tommyolivr@gmail.com>
2025-09-12 00:04:52 -0300

    fix db

--- src/db.js
+++ src/db.js
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
 import { createPool } from "mysql2/promise";
 
+dotenv.config();
+
 export const pool = createPool({
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_NAME || 'coupon_system',
+    database: process.env.DB_NAME,
     ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : undefined
 })
