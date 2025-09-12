198e8636bb2a9537b45d9a312af358241369c469
Tommy Oliver <tommyolivr@gmail.com>
2025-09-11 23:58:36 -0300

    fix db config

--- src/db.js
+++ src/db.js
@@ -3,8 +3,8 @@ import { createPool } from "mysql2/promise";
 export const pool = createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME || 'coupon_system',
     ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : undefined
 })
