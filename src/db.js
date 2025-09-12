10690146d1fd3d5940b82002958b3ec9c7683860
Tommy Oliver <tommyolivr@gmail.com>
2025-09-12 00:12:54 -0300

    fix db

--- src/db.js
+++ src/db.js
@@ -4,10 +4,12 @@ import { createPool } from "mysql2/promise";
 dotenv.config();
 
 export const pool = createPool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : undefined
-})
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true // 👈 Railway necesita SSL
+  }
+});
